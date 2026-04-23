a4eb223f0858f8f87d1f55997bd23b4c87845e90
Ricardo Ostette <ostettericardo@gmail.com>
2026-04-23 22:55:36 +0200

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ricardoostette/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ricardoostette.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ricardoostette.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
